16ed604332bf80bf5c7d1d86ca73e7a49d3bad76
Kevin <kevin.staeger@outlook.com>
2017-12-11 15:54:26 +0100

    audio

--- .demo/scripts/script.js
+++ .demo/scripts/script.js
@@ -1,5 +1,6 @@
-function QuizItem(question, variants, answer, enabled, replied, selectionOfUser) {
+function QuizItem(question, audio, variants, answer, enabled, replied, selectionOfUser) {
     this.question = question;
+    this.audio = audio;
     this.variants = variants;
     this.answer = answer;
     this.enabled = enabled;
@@ -8,17 +9,21 @@ function QuizItem(question, variants, answer, enabled, replied, selectionOfUser)
 }
 
 var quizQuestions = [];
-quizQuestions[0] = new QuizItem("Javascript has:",
+quizQuestions[0] = new QuizItem(
+    "Javascript has:",
+    'animal01.mp3',
     ["Function scope and block scope",
         "Just block scope",
         "Global and function scope",
         "Only global scope"],
-    2,
+    2, //Right answer
     false,
     false,
     undefined);
 
-quizQuestions[1] = new QuizItem("Which of these is a working object?",
+quizQuestions[1] = new QuizItem(
+    "Which of these is a working object?",
+    'animal01.mp3',
     ["var obj = { car: \"ford\" ; color:green }",
         "var obj = { car = \"ford\" , color = green }",
         "var obj = { car: \"ford\" , color:green }",
@@ -28,7 +33,9 @@ quizQuestions[1] = new QuizItem("Which of these is a working object?",
     false,
     undefined);
 
-quizQuestions[2] = new QuizItem("Which of these is a working loop?",
+quizQuestions[2] = new QuizItem(
+    "Which of these is a working loop?",
+    'animal01.mp3',
     ["for( i=0 i<10 i+=1) { console.log(i) }",
         "for (i=0; i<10; i+=1) {console.log(i)}",
         "for( i=0, i<10, i+=1) { console.log(i) }",
@@ -38,14 +45,18 @@ quizQuestions[2] = new QuizItem("Which of these is a working loop?",
     false,
     undefined);
 
-quizQuestions[3] = new QuizItem("What does this output:<br>'hello'!=123",
+quizQuestions[3] = new QuizItem(
+    "What does this output:<br>'hello'!=123",
+    'animal01.mp3',
     ["false", "true", "neither"],
     1,
     false,
     false,
     undefined);
 
-quizQuestions[4] = new QuizItem("var num = 10+''",
+quizQuestions[4] = new QuizItem(
+    "var num = 10+''",
+    'animal01.mp3',
     ["typeof num is number",
         "typeof numb is boolean",
         "typeof num is function",
@@ -61,8 +72,10 @@ quizQuestions[4] = new QuizItem("var num = 10+''",
 var currentIndex = 0, numOfAnswered = 0;
 var currentQuestion = quizQuestions[currentIndex];
 //second ulTag
+
 var ulTag = document.getElementsByTagName('ul')[1];
 var liTags = ulTag.getElementsByTagName('li');
+
 /*
 	this function inserts the current question into the layout
 	of the page: p tag which is a question and ul tag meaning
@@ -74,10 +87,14 @@ function showCurrentQuestion() {
     var numQuestion = parseInt(currentIndex)+1;
     headerOfDropdow.getElementsByTagName('span')[0].innerHTML = numQuestion;
     var pTag = document.getElementsByTagName('p')[0];
-    // console.log(liTags);
+    var audioSrc = document.getElementById('audiosrc');
+     // console.log(liTags);
+
+
     var ulTag = document.getElementsByTagName('ul')[1];
     var liTags = ulTag.getElementsByTagName('li');
     pTag.innerHTML = currentQuestion.question;
+    audioSrc.src = "content/audiofiles/"+ currentQuestion.audio;
     for (var i=0; i < liTags.length; i++) {
         //in case the number of variants is less than 4 (e.g. when it's
         // undefined) disable li tag
